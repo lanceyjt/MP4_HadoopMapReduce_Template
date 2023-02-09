9c34eac774964a735f2104e5f5382b146457b244
Jingtian Yao <jy2867@columbia.edu>
2023-02-08 22:46:28 -0800

    Implement TimeCount Mapper and Reducer

--- PythonTemplate/TitleCountMapper.py
+++ PythonTemplate/TitleCountMapper.py
@@ -12,20 +12,24 @@ delimitersPath = sys.argv[2]
 # TODO
 with open(stopWordsPath) as f:
     # TODO
-
-
-
-
-
+    stop_words = [line.strip() for line in f]
 
 #TODO 
 with open(delimitersPath) as f:
     # TODO
+    delimiters = f.read()
+    delimiters = [c for c in delimiters]
 
+default_delimiter = delimiters[0]
 for line in sys.stdin:
   
     # TODO
-
-    # print('%s\t%s' % (  ,  )) pass this output to reducer
+    line = line.strip()
+    for d in delimiters[1:]:
+        line = line.replace(d, default_delimiter)
+    word_lst = line.lower().split(default_delimiter)
+    word_lst = [w for w in word_lst if w not in stop_words]
+    for w in word_lst:
+        print('%s\t%s' % (w , 1)) # pass this output to reducer
 
 
--- PythonTemplate/TitleCountReducer.py
+++ PythonTemplate/TitleCountReducer.py
@@ -3,10 +3,31 @@ from operator import itemgetter
 import sys
 
 #TODO
+current_word = None
+current_count = 0
+word = None
 
 # input comes from STDIN
 for line in sys.stdin:
     # TODO
+    line = line.strip()
+    word, count = line.split('\t')
+    try:
+        count = int(count)
+    except ValueError:
+        continue
+    
+    if current_word == word:
+        current_count += count
+    else:
+        if current_word:
+            # write result to STDOUT
+            print('%s\t%s' % (current_word, current_count))
+        current_count = count
+        current_word = word
+
 
 # TODO
 # print('%s\t%s' % (  ,  )) print as final output
+if current_word == word:
+    print('%s\t%s' % (current_word, current_count))
