19c95cd3c8755089eda671d9958752b05ed30e90
Jingtian Yao <jy2867@columbia.edu>
2023-02-11 09:26:26 -0800

    Part C

--- PythonTemplate/OrphanPagesMapper.py
+++ PythonTemplate/OrphanPagesMapper.py
@@ -4,5 +4,11 @@ import sys
 
 for line in sys.stdin:
   # TODO
-  
+  line = line.strip()
+  source, dest = line.split(":")
+  source = source.strip()
+  dests = [x.strip() for x in dest.split(' ')]
+  for d in dests:
+      print('%s\t%s' % (source,d))
   # print('%s\t%s' % (  ,  )) pass this output to reducer
+
--- PythonTemplate/OrphanPagesReducer.py
+++ PythonTemplate/OrphanPagesReducer.py
@@ -3,10 +3,22 @@ import sys
 
 
 #TODO
-
+is_orphan_page = {}
+dests = []
 
 for line in sys.stdin:
   # TODO
+  line = line.strip()
+  source, dest = line.split('\t')
+  is_orphan_page[source] = True
+  dests.append(dest)
+
+for dest in dests:
+  is_orphan_page[dest] = all(is_orphan_page.get(dest, False), False)
+
+for (k,v) in is_orphan_page.items():
+  if v:
+    print(k)
 
 #TODO
 # print(xx) print as final output
