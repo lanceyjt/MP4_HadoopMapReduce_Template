028e5ee92d07c6ec0f52abc23b9dbd6331ce79f1
Jingtian Yao <jy2867@columbia.edu>
2023-02-11 08:54:34 -0800

    Part B

--- PythonTemplate/TopTitleStatisticsMapper.py
+++ PythonTemplate/TopTitleStatisticsMapper.py
@@ -4,5 +4,7 @@ import sys
 
 for line in sys.stdin:
     # TODO
-    
+    line = line.strip()
+    word, count = line.split('\t')
+    print('%s\t%s' % (word,count)) 
     # print('%s\t%s' % (  ,  )) pass this output to reducer
--- PythonTemplate/TopTitleStatisticsReducer.py
+++ PythonTemplate/TopTitleStatisticsReducer.py
@@ -4,9 +4,28 @@ import math
 
 
 #TODO
+counts = []
 
 for line in sys.stdin:
     # TODO
+    line = line.strip()
+    _, count = line.split('\t')
+    counts.append(int(count))
+
+
+sum_val = int(sum(counts))
+mean_val = int(sum_val/len(counts))
+min_val = int(min(counts))
+max_val = int(max(counts))
+
+temp_lst = [pow(x-mean_val, 2) for x in counts]
+var_val = int(sum(temp_lst)/len(counts))
 
 #TODO
 # print('%s\t%s' % (  ,  )) print as final output
+
+keys = ['Mean', 'Sum', 'Min', 'Max', 'Var']
+vals = [mean_val, sum_val, min_val, max_val, var_val]
+
+for i in range(5):
+    print('%s\t%s' % (keys[i],vals[i]))
