e966276635e3518e90d79bd6490f5caf9983a22e
Jingtian Yao <jy2867@columbia.edu>
2023-02-11 08:21:08 -0800

    Implement TopTitlesMapper and Reducer

--- PythonTemplate/TopTitlesMapper.py
+++ PythonTemplate/TopTitlesMapper.py
@@ -3,13 +3,19 @@ import sys
 
 
 # TODO
-
+word_freq = []
 
 
 for line in sys.stdin:
 
        #TODO
+       word, count = line.split('\t')
+       word_freq.append([word, count])
+
+word_freq.sort(reverse=False, key=lambda x: (x[1], x[0]))
 
 
 #TODO
 # print('%s\t%s' % (  ,  )) pass this output to reducer
+for x in word_freq[-10:]:
+       print('%s\t%s' % (x[0],x[1]))
--- PythonTemplate/TopTitlesReducer.py
+++ PythonTemplate/TopTitlesReducer.py
@@ -2,9 +2,15 @@
 import sys
 
 
+word_freq = []
 
 # input comes from STDIN
 for line in sys.stdin:
     # TODO
-    
+    word, count = line.split('\t')
+    word_freq.append([word, count])
+
     # print('%s\t%s' % (  ,  )) print as final output
+word_freq.sort(reverse=False, key=lambda x: (x[1], x[0]))
+for x in word_freq[-10:]:
+       print('%s\t%s' % (x[0],x[1]))
