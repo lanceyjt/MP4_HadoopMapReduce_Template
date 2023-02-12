d6ebe5f1e5ceae92a21af1ef996bfb03ebc26b1c
Jingtian Yao <jy2867@columbia.edu>
2023-02-12 11:55:10 -0800

    Fix part E

--- PythonTemplate/PopularityLeagueReducer.py
+++ PythonTemplate/PopularityLeagueReducer.py
@@ -14,7 +14,27 @@ for line in sys.stdin:
         continue
 
 #TODO
-page_count.sort(reverse=True, key=lambda x: x[0])
+page_count.sort(reverse=False, key=lambda x: x[1])
+diff = [page_count[i][1] - page_count[i-1][1] for i in range(1, len(page_count))]
+ranks = [0]
+for i in range(len(diff)):
+    current_rank = ranks[-1]
+    if diff[i] > 0:
+        ranks.append(len(ranks))
+    else:
+        ranks.append(current_rank)
+
+# page_rank = [[page_count[0][0], 0]]
+# current_rank = num = 0
+# for i in range(1, len(page_count)):
+#     if page_count[i][1] > page_count[i-1][1]:
+#         current_rank += 1
+#     num += 1
+#     page_rank.append([page_count[i][0], current_rank])
+
+pages = [x[0] for x in page_count]
+page_rank = list(zip(pages, ranks))
+page_rank.sort(reverse=True, key=lambda x: x[0])
 # print('%s\t%s' % (  ,  )) print as final output
-for p in page_count:
+for p in page_rank:
     print('%s\t%s' % (p[0],p[1]))
